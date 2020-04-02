9264fe62d6579cf09eebed4a0ca1ded7c9c83e5c
Vlad Doster <mvdoster@gmail.com>
2020-04-02 02:18:38 -0400

    disable antialias?

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "SauceCodePro Mono:pixelsize=16:antialias=true:autohint=true";
-static char *font2[] = { "SauceCodePro Mono:pixelsize=16:antialias=true:autohint=true" };
+static char *font = "SauceCodePro Mono:pixelsize=16:antialias=false:autohint=true";
+static char *font2[] = { "SauceCodePro Mono:pixelsize=16:antialias=false:autohint=true" };
 static int borderpx = 1;
 
 /*
