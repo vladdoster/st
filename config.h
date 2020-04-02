fb4836352a6d399eff2a3c6b5ca690791299f31f
Vlad Doster <mvdoster@gmail.com>
2020-04-02 02:06:16 -0400

    Trying to pinpoint issue

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "SauceCodePro:pixelsize=16:antialias=true:autohint=true";
-static char *font2[] = { "JoyPixels:pixelsize=16:antialias=true:autohint=true" };
+static char *font2[] = { "SauceCodePro:pixelsize=16:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
