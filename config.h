ed58073ab15185185f09a5fdbee095732c1ca587
Vlad Doster <mvdoster@gmail.com>
2020-04-02 02:18:01 -0400

    change shell back to sh and try adding Mono to font

--- config.h
+++ config.h
@@ -5,10 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-// static char *font = "SauceCodePro:pixelsize=16:antialias=true:autohint=true";
-// static char *font2[] = { "SauceCodePro:pixelsize=16:antialias=true:autohint=true" };
-static char *font = "Liberation Mono:pixelsize=16:antialias=false:autohint=true";
-static char *font2[] = { "Liberation Mono:pixelsize=16:antialias=true:autohint=true" };
+static char *font = "SauceCodePro Mono:pixelsize=16:antialias=true:autohint=true";
+static char *font2[] = { "SauceCodePro Mono:pixelsize=16:antialias=true:autohint=true" };
 static int borderpx = 1;
 
 /*
@@ -19,7 +17,7 @@ static int borderpx = 1;
  * 4: value of shell in /etc/passwd
  * 5: value of shell in config.h
  */
-static char *shell = "/bin/zsh";
+static char *shell = "/bin/sh";
 char *utmp = NULL;
 char *stty_args = "stty raw pass8 nl -echo -iexten -cstopb 38400";
 
