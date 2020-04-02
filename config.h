cf879d4267c706defa05ba420c3d421aafa9a64c
Vlad Doster <mvdoster@gmail.com>
2020-04-02 02:08:52 -0400

    still fixing it

--- config.h
+++ config.h
@@ -5,8 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "SauceCodePro:pixelsize=16:antialias=true:autohint=true";
-static char *font2[] = { "SauceCodePro:pixelsize=16:antialias=true:autohint=true" };
+// static char *font = "SauceCodePro:pixelsize=16:antialias=true:autohint=true";
+// static char *font2[] = { "SauceCodePro:pixelsize=16:antialias=true:autohint=true" };
+static char *font = "Liberation Mono:pixelsize=12:antialias=false:autohint=true";
 static int borderpx = 2;
 
 /*
