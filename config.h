22af7b1e2b503222561e6b3b30092762f9ecfccb
Vlad Doster <mvdoster@gmail.com>
2020-04-02 02:28:37 -0400

    fixed type in font 2

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "SourceCodePro:pixelsize=16:antialias=true:autohint=true";
-static char *font2[] = { "SSourceCodePro:pixelsize=16:antialias=true:autohint=true" };
+static char *font2[] = { "SourceCodePro:pixelsize=16:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
